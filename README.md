5da2d601f46fe0315834c071391f5731a35d1255
funda-yesiltas <73352224+funda-yesiltas@users.noreply.github.com>
2021-05-17 22:59:52 +0200

    Vorlage Projekt

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Internetprogrammierung-Projekt
+![GitHub Classroom Workflow](https://github.com/hft-stuttgart-ipr/hft-asgmt-js-async-funda-yesiltas/workflows/GitHub%20Classroom%20Workflow/badge.svg)
+
+
+# hft-asgmt-js-async
+
+## TODO
+  - [x] Index.ejs should display the form to add new shouts and also the table of all shouts
+  - [x] Create a route, for `GET /api/shouts` to get a JSON response with all shouts
+  - [x] Create a route, for `POST /api/shouts` to receive JSON and add a new shout
+  - [x] The homepage must not reload!
+
+The Json must look like:
+
+```js
+{
+  username: 'some string',
+  message: 'another string',
+}
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser')
+const db = new sqlite3.Database('./db/shoutbox.db');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get("/", async (req, res) => {
+  db.all("SELECT * FROM shouts", (err, shouts) => {
+    res.render("pages/index", { shouts })
+  });
+});
+
+app.get("/api/shouts", async (req, res) => {
+  db.all("SELECT * FROM shouts", (err, shouts) => {
+    res.json(shouts);
+  });
+});
+
+app.post('/api/shouts', (req, res) => {
+  if (req.body.username && req.body.message) {
+    db.run('INSERT INTO shouts(username, message) VALUES (?, ?);', [req.body.username, req.body.message], function (err) {
+      if(err) {
+        res.json({error: err});
+      } else {
+        res.json({
+          ...req.body,
+          id: this.lastID,
+
+        });
+      }
+    });
+  } else {
+    res.json({error:"Request body is not correct"});  
+  }
+});
+
+const server = app.listen(port, () => {
+ console.log(`Server listening on port ${port}…`)
+});
+
+module.exports = server
